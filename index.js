205116532e26f6253ef311e28e649d01193239c9
uliguapo1 <ulisescamelo933@gmail.com>
2025-07-16 08:40:44 -0600

    staert of proyect

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require ('express');
+const app = express();
+const port = 3000;
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.get("/", function(req, res){
+    res.render('index.ejs');
+
+});
